14931c331b1a7ae632ac80edf30465ba63fb4ba1
eyvmal <eyvind.malde@gmail.com>
2023-10-25 17:59:33 +1000

    integrate helper_functions.h and datagram.h

--- source/callpoint.c
+++ source/callpoint.c
@@ -17,77 +17,63 @@ Make sure to link with the pthread library (-lpthread) when compiling this progr
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-// Shared memory structure
-struct shared_memory {
-    char status; // '-' for inactive, '*' for active
-    pthread_mutex_t mutex; // Only one process can access status at a time
-    pthread_cond_t cond; // Allows threads to wait until status is '*'
-};
+#include "helper_functions.h"
+#include "shm_structs.h"
+#include "dg_structs.h"
 
-// Datagram format
-struct datagram {
-    char header[4]; // {'F', 'I', 'R', 'E'}
-};
-
-void send_fire_datagram(const char *addr, int port) {
-    struct sockaddr_in destination;
-    struct datagram fire = {{'F', 'I', 'R', 'E'}};
-
-    // socket(int family, int type, int protocol)
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock == -1) {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    memset(&destination, 0, sizeof(destination));
-    destination.sin_family = AF_INET;
-    destination.sin_port = htons(port);
-    inet_pton(AF_INET, addr, &destination.sin_addr);
-
-    sendto(sock, &fire, sizeof(fire), 0, (struct sockaddr*)&destination, sizeof(destination));
-    close(sock);
+void send_fire_datagram(int sock, const struct sockaddr_in *destination)
+{
+  fire_emergency_dg fire = {{'F', 'I', 'R', 'E'}};
+  sendto(sock, &fire, sizeof(fire), 0, (struct sockaddr *)destination, sizeof(*destination));
 }
 
-int main(int argc, char *argv[]) {
-    if(argc != 5) {
-        fprintf(stderr, "Usage: %s <resend delay (in microseconds)> <shared memory path> <shared memory offset> <fire alarm unit address:port>\n", argv[0]);
-        exit(1);
+int main(int argc, char *argv[])
+{
+  // Check that there are correct amount of args
+  if (argc != 5)
+  {
+    fprintf(stderr, "Usage: %s <resend delay (in microseconds)> <shared memory path> <shared memory offset> <fire alarm unit address:port>\n", argv[0]);
+    exit(1);
+  }
+
+  // Save the command line args to local variables
+  int resend_delay = atoi(argv[1]);
+  const char *shm_path = argv[2];
+  int offset = atoi(argv[3]);
+  char *address = argv[4];
+
+  // Open shared memory using helper_functions
+  char *shm = open_shared_memory(shm_path);
+  shm_callpoint *shared = (shm_callpoint *)(shm + offset);
+
+  // Split the port from the address and save both in their own variable
+  char addr_str[100], *port_str;
+  strcpy(addr_str, argv[4]);
+  port_str = strchr(addr_str, ':');
+  *port_str++ = '\0';
+  int port = atoi(port_str);
+
+  // Create a UDP socket
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  struct sockaddr_in destination = {0};
+  destination.sin_family = AF_INET;
+  destination.sin_port = htons(port);
+  inet_pton(AF_INET, addr_str, &destination.sin_addr);
+
+  while (1)
+  {
+    pthread_mutex_lock(&shared->mutex);
+    if (shared->status == '*')
+    {
+      send_fire_datagram(sock, &destination);
+      usleep(resend_delay);
     }
-
-    int resend_delay = atoi(argv[1]);
-    const char *shm_path = argv[2];
-    int offset = atoi(argv[3]);
-    char *address = strtok(argv[4], ":");
-
-    int shm_fd = shm_open(shm_path, O_RDWR, 0666);
-    if(shm_fd == -1) {
-        perror("Failed to open shared memory");
-        exit(1);
+    else
+    {
+      pthread_cond_wait(&shared->cond, &shared->mutex);
     }
-
-    struct shared_memory *shm_ptr = mmap(NULL, sizeof(struct shared_memory), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, offset);
-    if(shm_ptr == MAP_FAILED) {
-        perror("Failed to map shared memory");
-        close(shm_fd);
-        exit(1);
-    }
-
-    while(1) {
-        pthread_mutex_lock(&shm_ptr->mutex);
-
-        if(shm_ptr->status == '*') {
-            send_fire_datagram(address, port);
-            usleep(resend_delay);
-        } else {
-            pthread_cond_wait(&shm_ptr->cond, &shm_ptr->mutex);
-        }
-
-        pthread_mutex_unlock(&shm_ptr->mutex);
-    }
-
-    // This code should ideally never reach here in normal operation.
-    munmap(shm_ptr, sizeof(struct shared_memory));
-    close(shm_fd);
-    return 0;
+    pthread_mutex_unlock(&shared->mutex);
+  }
+  close(sock);
+  return 0;
 }
