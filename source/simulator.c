48cbc662c308f6eff2f285ce55b617552344b06a
eyvmal <eyvind.malde@gmail.com>
2023-10-25 16:56:08 +1000

    add line-reader

--- source/simulator.c
+++ source/simulator.c
@@ -5,7 +5,8 @@
 #include <pthread.h>
 
 // Define shared memory structures for different components
-typedef struct {
+typedef struct
+{
     // Define data for the overseer component
     // ...
 
@@ -17,28 +18,60 @@ typedef struct {
 // ...
 
 // Initialize shared memory and mutexes
-void initializeSharedMemory() {
+void initializeSharedMemory()
+{
     // Initialize shared memory for each component
     // Initialize mutexes and condition variables
     // ...
 }
 
 // Simulate an event for a specific component
-void simulateEvent(OverseerData* overseer, int componentID) {
+void simulateEvent(OverseerData *overseer, int componentID)
+{
     // Implement the logic for simulating different events
     // You can use mutexes and condition variables to synchronize
     // with the component's behavior.
     // ...
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Usage: %s <scenario_file>\n", argv[0]);
         return 1;
     }
 
-    // Parse the scenario file and read events
-    // ...
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        perror("Failed to open file");
+        return 2;
+    }
+
+    char line[1024];
+    while (fgets(line, sizeof(line), file))
+    {
+        char command[64], type[64];
+        sscanf(line, "%s %s", command, type);
+
+        if (strcmp(command, "INIT") == 0)
+        {
+            if (strcmp(type, "cardreader") == 0)
+            {
+                // {id} {wait time (in microseconds)} {shared memory path} {shared memory offset} {overseer address:port}
+                int id, waittime, offset;
+                char path, adress_string;
+                sscanf(line, "%*s %*s %d %d %s %d %s", &id, &waittime, &path, &offset, &adress_string); // %*s reads and discards a string
+                initiateCardreader(id, waittime, path, offset, adress_string);
+            }
+            // Other types can be added here, similar to the above check
+            // For example:
+            // else if (strcmp(type, "door") == 0) { ... }
+            // else if (strcmp(type, "overseer") == 0) { ... }
+            // etc.
+        }
+    }
 
     // Initialize shared memory and components
     initializeSharedMemory();
@@ -52,5 +85,6 @@ int main(int argc, char* argv[]) {
     // Terminate processes and release shared memory
     // ...
 
+    fclose(file);
     return 0;
 }
