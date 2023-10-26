f51c925c15adee7ff6f626e5f2e272337e78245e
Håkon Junge <haakonjunge@icloud.com>
2023-10-26 16:47:36 +1000

    main

--- source/main.c
+++ source/main.c
@@ -3,12 +3,13 @@
 #include "overseer.c"
 
 int main(int argC, char *argV[]){
-    /* char *arg[] = {"processName", "overseer", "127.0.0.1:3001", "FAIL_SAFE", "shm_path", "shm_offset", "127.0.0.1:3000", NULL};
+    
+    char *arg[] = {"processName", "overseer", "127.0.0.1:3001", "FAIL_SAFE", "shm_path", "shm_offset", "127.0.0.1:3000", NULL};
     printStrings(&arg[2]);
     char *arg[] = {"processName", "overseer", "127.0.0.1:3000", "2000", "250", "auth_file", "connection_file", "layout_file" ,"shm_path", "shm_offset"};
     if(arg[1] == "overseer"){
         overseer(argC-2, &arg[2]);
-    } */
+    }
 
 
     char *arg1[] = {"processName", "door", "127.0.0.1:3001", "FAIL_SAFE", "shm_path", "shm_offset", "127.0.0.1:300"};
