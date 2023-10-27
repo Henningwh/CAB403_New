bfbde16fad7d13b09a3df747f018835ec09b422b
eyvmal <eyvind.malde@gmail.com>
2023-10-27 17:59:48 +1000

    add SCENARIO support

--- source/simulator.c
+++ source/simulator.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <signal.h>
 
-#include "shm_structs.h"
+#include "data_structs.h"
 #include "dg_structs.h"
 #include "helper_functions.h"
 
@@ -15,210 +15,8 @@
 #define BASE_ADDRESS "127.0.0.1"
 #define BASE_PORT 3000
 
-// To keep track of how many of each component we have spawned
-typedef struct
-{
-    int overseer_count;
-    int firealarm_count;
-    int cardreader_count;
-    int door_count;
-    int callpoint_count;
-    int tempsensor_count;
-    int elevator_count;
-    int destselect_count;
-} instance_counters;
 instance_counters counters = {0}; // Set them all to 0 as default
-
-typedef struct
-{
-    pid_t overseer;
-    pid_t firealarm;
-    pid_t cardreader[40];
-    pid_t door[20];
-    pid_t callpoint[20];
-    pid_t tempsensor[20];
-    pid_t elevator[10];
-    pid_t destselect[20];
-} ProcessPIDs;
-ProcessPIDs processPIDs = {0}; // Initialize all PIDs to 0
-
-// The maximum size the shared memory can be
-typedef struct
-{
-    shm_overseer overseer;
-    shm_firealarm firealarm;
-    shm_cardreader cardreader[40];
-    shm_door door[20];
-    shm_callpoint callpoint[20];
-    shm_tempsensor tempsensor[20];
-    shm_elevator elevator[10];
-    shm_destselect destselect[20];
-    shm_camera camera[20];
-} shm_master;
-
-typedef struct
-{
-    void *base;
-    shm_overseer *pOverseer;
-    shm_firealarm *pFirealarm;
-    shm_cardreader *pCardreader;
-    shm_door *pDoor;
-    shm_callpoint *pCallpoint;
-    shm_tempsensor *pTempsensor;
-    shm_elevator *pElevator;
-    shm_destselect *pDestselect;
-} ShmPointers;
-
-size_t calculateTotalShmSize()
-{
-    return sizeof(shm_overseer) + sizeof(shm_firealarm) + (sizeof(shm_cardreader) * 40) +
-           (sizeof(shm_door) * 20) + (sizeof(shm_callpoint) * 20) + (sizeof(shm_tempsensor) * 20) +
-           (sizeof(shm_elevator) * 10) + (sizeof(shm_destselect) * 20) + (sizeof(shm_camera) * 20);
-}
-
-ShmPointers initializeSharedMemory()
-{
-    ShmPointers pointers;
-    size_t offset = 0;
-
-    // Initialize shared memory space
-    int fd = shm_open("/shm", O_CREAT | O_RDWR, 0);
-    if (fd == -1)
-    {
-        perror("Error: An error occurred in shm_open()");
-        exit(1);
-    }
-
-    size_t size = calculateTotalShmSize();
-    printf("Total shared memory size: %zu bytes (Should be 54592)\n", size);
-    if (ftruncate(fd, size) == -1)
-    {
-        perror("Error: An error occurred in ftruncate()");
-        close(fd);
-        exit(1);
-    }
-
-    void *master = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (master == MAP_FAILED)
-    {
-        perror("Error: An error occurred in mmap()");
-        close(fd);
-        exit(1);
-    }
-    memset(master, 0, size);
-    close(fd);
-
-    // Base
-    pointers.base = master;
-
-    // Overseer
-    pointers.pOverseer = (shm_overseer *)((char *)master + offset);
-    // Firealarm
-    offset += sizeof(shm_overseer);
-    pointers.pFirealarm = (shm_firealarm *)((char *)master + offset);
-    // Cardreader
-    offset += sizeof(shm_firealarm);
-    pointers.pCardreader = (shm_cardreader *)((char *)master + offset);
-    // Door
-    offset += sizeof(shm_cardreader) * 40;
-    pointers.pDoor = (shm_door *)((char *)master + offset);
-    // Callpoint
-    offset += sizeof(shm_door) * 20;
-    pointers.pCallpoint = (shm_callpoint *)((char *)master + offset);
-    // Tempsensor
-    offset += sizeof(shm_callpoint) * 20;
-    pointers.pTempsensor = (shm_tempsensor *)((char *)master + offset);
-    // Elevator
-    offset += sizeof(shm_tempsensor) * 20;
-    pointers.pElevator = (shm_elevator *)((char *)master + offset);
-    // Destselect
-    offset += sizeof(shm_elevator) * 10;
-    pointers.pDestselect = (shm_destselect *)((char *)master + offset);
-
-    return pointers;
-}
-
-void cleanupSharedMemory()
-{
-    shm_unlink("/shm");
-}
-
-pid_t spawn_process(const char *process_path, char *const argv[])
-{
-    pid_t processPID = fork();
-
-    if (processPID == 0)
-    {
-        // Child process
-        usleep(250000);
-        execv(process_path, argv);
-        // If execv() fails:
-        perror("Error: execv() failed to launch process");
-        exit(1);
-    }
-    else if (processPID < 0)
-    {
-        perror("Error: fork() failed");
-        exit(1);
-    }
-    // Parent process continues here:
-    return processPID;
-}
-
-void terminate_all_processes(ProcessPIDs *pids)
-{
-    int i;
-
-    if (pids->overseer)
-    {
-        printf("Killing process: %d\n", pids->overseer);
-        kill(pids->overseer, SIGTERM);
-    }
-    if (pids->firealarm)
-    {
-        printf("Killing process: %d\n", pids->firealarm);
-        kill(pids->firealarm, SIGTERM);
-    }
-    for (i = 0; i < 40; i++)
-    {
-        if (pids->cardreader[i])
-        {
-            printf("Killing process: %d\n", pids->cardreader[i]);
-            kill(pids->cardreader[i], SIGTERM);
-        }
-    }
-    for (i = 0; i < 20; i++)
-    {
-        if (pids->door[i])
-        {
-            printf("Killing process: %d\n", pids->door[i]);
-            kill(pids->door[i], SIGTERM);
-        }
-        if (pids->callpoint[i])
-        {
-            printf("Killing process: %d\n", pids->callpoint[i]);
-            kill(pids->callpoint[i], SIGTERM);
-        }
-        if (pids->tempsensor[i])
-        {
-            printf("Killing process: %d\n", pids->tempsensor[i]);
-            kill(pids->tempsensor[i], SIGTERM);
-        }
-        if (pids->destselect[i])
-        {
-            printf("Killing process: %d\n", pids->destselect[i]);
-            kill(pids->destselect[i], SIGTERM);
-        }
-    }
-    for (i = 0; i < 10; i++)
-    {
-        if (pids->elevator[i])
-        {
-            printf("Killing process: %d\n", pids->elevator[i]);
-            kill(pids->elevator[i], SIGTERM);
-        }
-    }
-}
+ProcessPIDs processPIDs = {0};    // Initialize all PIDs to 0
 
 int main(int argc, char *argv[])
 {
@@ -242,6 +40,7 @@ int main(int argc, char *argv[])
     }
 
     // Read the file
+    printf("--- Creating Processes: ---\n");
     char line[1024];
     while (fgets(line, sizeof(line), file))
     {
@@ -430,12 +229,53 @@ int main(int argc, char *argv[])
                 counters.destselect_count++;
             }
         }
+        else if (strcmp(command, "SCENARIO") == 0)
+        {
+            usleep(1000000); // 1 sec sleep
+            printf("--- SCENARIOS: ---\n");
+            while (fgets(line, sizeof(line), file))
+            {
+                long timestamp;
+                char event_type[64];
+                int num;
+
+                sscanf(line, "%ld %s %d", &timestamp, event_type, &num);
+
+                if (strcmp(event_type, "CARD_SCAN") == 0)
+                {
+                    int code;
+                    sscanf(line, "%*ld %*s %*d %d", &code);
+                    printf("%ld %s %d %d\n", timestamp, event_type, num, code);
+                }
+                else if (strcmp(event_type, "CALLPOINT_TRIGGER") == 0)
+                {
+                    printf("%ld %s %d\n", timestamp, event_type, num);
+                }
+                else if (strcmp(event_type, "TEMP_CHANGE") == 0)
+                {
+                    float new_temperature;
+                    sscanf(line, "%*ld %*s %*d %f", &new_temperature);
+                    printf("%ld %s %d %.1f\n", timestamp, event_type, num, new_temperature);
+                }
+                else if (strcmp(event_type, "DEST_SELECT") == 0)
+                {
+                    int code, floor;
+                    sscanf(line, "%*ld %*s %*d %d %d", &code, &floor);
+                    printf("%ld %s %d %d %d\n", timestamp, event_type, num, code, floor);
+                }
+                else
+                {
+                    fprintf(stderr, "Unknown event type in SCENARIO section: %s\n", event_type);
+                }
+            }
+        }
     }
 
     // Simulate events based on the scenario
     // ...
 
     // Terminate processes, release shared memory and close the file
+    usleep(1000000); // 1 sec sleep
     terminate_all_processes(&processPIDs);
     cleanupSharedMemory();
     fclose(file);
